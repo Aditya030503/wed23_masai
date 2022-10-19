179ccd95c247334aa848ffd9ac2fd472c9ab29a1
Aditya <115468905+Aditya030503@users.noreply.github.com>
2022-10-19 12:22:52 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,2 +1,7 @@
 let email="abc32@gmail.com";
 let pass="123123";
+let enter_email="abc32@gmail.com";
+let enter_pass="12312";
+if(email==enter_email){
+  console.log("login successfully");}
+else{console.log("login fail");}
