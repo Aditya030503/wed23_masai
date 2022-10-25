b948d5a9ad2c81140b0e9b422922876c1f903a09
Aditya <115468905+Aditya030503@users.noreply.github.com>
2022-10-26 00:21:06 +0530

    Revert " login succesfully added"

--- abc.js
+++ abc.js
@@ -2,9 +2,6 @@ let email="abc32@gmail.com";
 let pass="123123";
 let enter_email="abc32@gmail.com";
 let enter_pass="12312";
-if(email===enter_email && pass===enter_pass){
-  console.log("login successfully");
-}
-else{
-console.log("login fail");
-}
+if(email==enter_email){
+  console.log("login successfully");}
+else{console.log("login fail");}
